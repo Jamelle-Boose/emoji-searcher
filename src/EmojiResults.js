3bb89f5450a9c1532034c93b26318d091a898a55
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-03-18 19:04:54 -0700

    dynamic-layout: add JSX loops

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -3,12 +3,27 @@ import EmojiResultRow from './EmojiResultRow';
 
 class EmojiResults extends React.Component {
   render() {
+    const { emojiData } = this.props;
+
     return (
       <div>
-        <EmojiResultRow title='Grinning' symbol='😀' />
+        {/* <EmojiResultRow title='Grinning' symbol='😀' /> */}
+        {emojiData.map(emojiData => {
+          return (
+            <EmojiResultRow
+              key={emojiData.symbol}
+              symbol={emojiData.symbol}
+              title={emojiData.title}
+            />
+          );
+        })}
       </div>
     );
   }
 }
+
+EmojiResults.propTypes = {
+  emojiData: React.PropTypes.array
+};
 
 export default EmojiResults;
